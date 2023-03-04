f0843bbff18b34016b790f5a689d211887e63929
tsyganno <tsyganlee@gmail.com>
2023-03-04 21:19:18 +0800

    add return in first tochka

--- telinder.py
+++ telinder.py
@@ -17,111 +17,80 @@ logging.basicConfig(level=logging.INFO)
 
 API_TOKEN = getenv('BOT_TOKEN')
 
-
 bot = Bot(token=API_TOKEN)
 
-# Например, используйте простой MemoryStorage для Dispatcher.
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
 
-# Состояния
 class Form(StatesGroup):
-    name = State()  # Будет представлен в хранилище как «Форма: имя».
-    user_photo = State()  # Будет представлен в хранилище как «Форма: фото».
-    age = State()  # Будет представлен в хранилище как «Форма: возраст».
-    gender = State()  # Будет представлен в хранилище как «Форма: пол».
+    name = State()
+    user_photo = State()
+    age = State()
+    gender = State()
 
 
 @dp.message_handler(commands='start')
 async def cmd_start(message: types.Message):
-    """
-    Точка входа в разговор
-    """
-    # Установить состояние
+    """ Точка входа в разговор """
     await Form.name.set()
-
     await message.reply("Всем привет! Как тебя зовут?")
 
 
-# Вы можете использовать состояние '*', если вам нужно обрабатывать все состояния
-@dp.message_handler(state='*', commands='cancel')
-@dp.message_handler(Text(equals='cancel', ignore_case=True), state='*')
-async def cancel_handler(message: types.Message, state: FSMContext):
-    """
-    Разрешить пользователю отменять любое действие
-    """
-    current_state = await state.get_state()
-    if current_state is None:
-        return
-
-    logging.info('Cancelling state %r', current_state)
-    # Отменить состояние и сообщить об этом пользователю
-    await state.finish()
-    # И убрать клавиатуру (на всякий случай)
-    await message.reply('Отменено.', reply_markup=types.ReplyKeyboardRemove())
+@dp.message_handler(state='*', commands=['no'])
+async def process_name(message: types.Message, state: FSMContext):
+    """ Возврат в исходное состояние, в точку входа в разговор """
+    await Form.name.set()
+    await message.reply("Всем привет! Как тебя зовут?")
 
 
 @dp.message_handler(state=Form.name)
 async def process_name(message: types.Message, state: FSMContext):
-    """
-    Имя пользователя процесса
-    """
+    """ Имя пользователя процесса """
     async with state.proxy() as data:
         data['name'] = message.text
-
     await Form.next()
     await message.reply("Добавьте свое фото")
 
 
 @dp.message_handler(lambda message: not message.photo, state=Form.user_photo)
 async def process_photo_invalid(message: types.Message):
-    """
-    Если пользователь не загружает фото
-    """
+    """ Если пользователь загружает не фото """
     return await message.reply("Фотография не найдена в сообщении =(\nЗагрузи свою фотку")
 
 
 @dp.message_handler(content_types=['photo'], state=Form.user_photo)
 async def process_photo(message: types.Message, state: FSMContext):
+    """ Фото пользователя процесса """
     async with state.proxy() as data:
         document_id = message.photo[-1].file_id
         file_info = await bot.get_file(document_id)
         path = f'http://api.telegram.org/file/bot{API_TOKEN}/{file_info.file_path}'
         data['user_photo'] = path
-
     await Form.next()
     await message.reply("Сколько тебе лет?")
 
 
-# Check age. Age gotta be digit
 @dp.message_handler(lambda message: not message.text.isdigit(), state=Form.age)
 async def process_age_invalid(message: types.Message):
-    """
-    Если возраст неверный
-    """
+    """ Если возраст у пользователя неверный """
     return await message.reply("Возраст должен быть числом.\nСколько тебе лет? (только цифры)")
 
 
 @dp.message_handler(lambda message: message.text.isdigit(), state=Form.age)
 async def process_age(message: types.Message, state: FSMContext):
-    # Update state and data
+    """ Выбор взраста пользователем """
     await Form.next()
     await state.update_data(age=int(message.text))
-
-    # Configure ReplyKeyboardMarkup
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
     markup.add("Мужчина", "Женщина")
     markup.add("Другие")
-
     await message.reply("Какого Вы пола?", reply_markup=markup)
 
 
-@dp.message_handler(lambda message: message.text not in ["Мужчина", "Женщина", "Другие"], state=Form.gender)
+@dp.message_handler(lambda message: message.text not in ["Мужчина", "Женщина"], state=Form.gender)
 async def process_gender_invalid(message: types.Message):
-    """
-    В этом примере пол должен быть одним из: Мужской, Женский, Другой.
-    """
+    """В этом примере пол должен быть одним из: Мужской или Женский """
     return await message.reply("Плохое гендерное имя. Выберите свой пол с клавиатуры.")
 
 
@@ -129,11 +98,7 @@ async def process_gender_invalid(message: types.Message):
 async def process_gender(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['gender'] = message.text
-
-        # Remove keyboard
         markup = types.ReplyKeyboardRemove()
-
-        # And send message
         await bot.send_message(
             message.chat.id,
             md.text(
@@ -145,8 +110,6 @@ async def process_gender(message: types.Message, state: FSMContext):
             reply_markup=markup,
             parse_mode=ParseMode.MARKDOWN,
         )
-
-    # Finish conversation
     await state.finish()
 
 
